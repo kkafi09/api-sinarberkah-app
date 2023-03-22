98085a5611adf930c5cb1eb80801767ebfee1ba4
kkafi09 <afi.firda@gmail.com>
2023-03-22 22:51:28 +0700

    vercel: trigger and make gallery without jwt

--- api/gallery.js
+++ api/gallery.js
@@ -7,20 +7,12 @@ const jwtAuth = require("../middlewares/jwtAuth");
 
 const uploader = multer();
 
-router.post(
-  "/",
-  [jwtAuth.verifyToken, uploader.single("image")],
-  galleryController.createGallery
-);
+router.post("/", [uploader.single("image")], galleryController.createGallery);
 
 router.get("/", galleryController.getGalleries);
 
 router.get("/:category", galleryController.getGalleryByCategory);
 
-router.delete(
-  "/:galleryId",
-  jwtAuth.verifyToken,
-  galleryController.deleteGallery
-);
+router.delete("/:galleryId", galleryController.deleteGallery);
 
 module.exports = router;
